337a17f277d2cbebdfc93f56add57b1d8ac43610
Yi-An Chen <yachen@nuvoton.com>
2017-02-22 13:21:02 +0800

    Disable write protect mode to write wDT control register
    Change-Id: I56097087d3b4dad32b084b88f830ac5887bb9c26

--- SampleCode/WDT/main.c
+++ SampleCode/WDT/main.c
@@ -25,14 +25,20 @@ int main(void)
     // Disable all interrupts.
     outpw(REG_AIC_MDCR, 0xFFFFFFFE);
     outpw(REG_AIC_MDCRH, 0x3FFFFFFF);
-    
+
     sysDisableCache();
     sysFlushCache(I_D_CACHE);
     sysEnableCache(CACHE_WRITE_BACK);
     sysInitializeUART();
-    
+
     sysprintf("\nThis sample code demonstrate reset WDT function\n");
-    
+
+    // Disable write protect mode to control WDT register
+    outpw(REG_SYS_REGWPCTL,0x59);
+    outpw(REG_SYS_REGWPCTL,0x16);
+    outpw(REG_SYS_REGWPCTL,0x88);
+    while(!(inpw(REG_SYS_REGWPCTL)&(1<<0)));
+
     outpw(REG_CLK_PCLKEN0, inpw(REG_CLK_PCLKEN0) | 1); // Enable WDT engine clock
 
     sysSetWatchDogTimerInterval(6);     // Set WDT time out interval to 2^16 Twdt = 0.7 sec. Where Twdt = 12MHZ / 128
